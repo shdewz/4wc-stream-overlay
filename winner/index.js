3bcc00fad17eec221197ba97eedbfe8bdfd01123
shdewz <shdewz@gmail.com>
2025-05-14 18:55:58 +0300

    Update winner scene to 2025

--- winner/index.js
+++ winner/index.js
@@ -7,29 +7,6 @@ let comingup, teams, mappool;
 	if (mappool?.stage) $('#title').text(mappool.stage.toUpperCase());
 })();
 
-const update_match = match => {
-	$('#title').text('COMING UP')
-	const red_team = teams.find(team => team.team === match.red_team);
-	const blue_team = teams.find(team => team.team === match.blue_team);
-	update_team('red', red_team);
-	update_team('blue', blue_team);
-
-	if (match.time > Date.now()) {
-		let timer_int = setInterval(() => {
-			if (match.time < Date.now()) {
-				clearInterval(timer_int);
-				$('#timer').text('00:00');
-			}
-			let remaining = Math.floor((match.time - Date.now()) / 1000);
-			let hours = Math.floor(remaining / 60 / 60);
-			let date = new Date(null);
-			date.setSeconds(remaining);
-			let text = hours > 0 ? date.toISOString().slice(11, 19) : date.toISOString().slice(14, 19);
-			if (timer && remaining > 0) $('#timer').text(text);
-		}, 1000);
-	}
-}
-
 const update_team = (color, team) => {
 	$(`#name_${color}`).text(team.team);
 	$(`#flag_${color}`).css('background-image', `url('../_shared/assets/flags/${team.flag}.png')`);
@@ -50,17 +27,7 @@ socket.onerror = error => { console.log('Socket Error: ', error); };
 let artist, title;
 let points_r, points_b;
 socket.onmessage = async event => {
-	let data = JSON.parse(event.data);
-
-	if (artist !== data.menu.bm.metadata.artist || title !== data.menu.bm.metadata.title) {
-		artist = data.menu.bm.metadata.artist;
-		title = data.menu.bm.metadata.title;
-		$('#song_title_container').css('opacity', 0);
-		await delay(300);
-		$('#song_artist').text(artist);
-		$('#song_title').text(title);
-		$('#song_title_container').css('opacity', 1);
-	}
+	const data = JSON.parse(event.data);
 
 	if (teams && (points_r !== data.tourney.manager.stars.left || points_b !== data.tourney.manager.stars.right)) {
 		points_r = data.tourney.manager.stars.left;
