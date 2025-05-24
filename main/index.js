50de981963addbde830a95b66562cd4d14929c53
shdewz <shdewz@gmail.com>
2025-05-24 19:02:47 +0300

    Cap OD at 10 before applying speed rate

--- main/index.js
+++ main/index.js
@@ -373,10 +373,10 @@ const getModStats = (cs_raw, ar_raw, od_raw, bpm_raw, mods) => {
 	const ar_ms = Math.max(Math.min(ar <= 5 ? 1800 - 120 * ar : 1200 - 150 * (ar - 5), 1800), 450) / speed;
 	ar = ar < 5 ? (1800 - ar_ms) / 120 : 5 + (1200 - ar_ms) / 150;
 
-	const cs = Math.round(Math.min(mods.includes('HR') ? cs_raw * 1.3 : mods.includes('EZ') ? cs_raw * 0.5 : cs_raw, 10) * 10) / 10;
+	const cs = mods.includes('HR') ? cs_raw * 1.3 : mods.includes('EZ') ? cs_raw * 0.5 : cs_raw;
 
-	let od = mods.includes('HR') ? od_raw * 1.4 : mods.includes('EZ') ? od_raw * 0.5 : od_raw;
-	if (speed !== 1) od = Math.round(Math.min((79.5 - Math.min(79.5, Math.max(19.5, 79.5 - Math.ceil(6 * od))) / speed) / 6, 11) * 10) / 10;
+	let od = Math.min(mods.includes('HR') ? od_raw * 1.4 : mods.includes('EZ') ? od_raw * 0.5 : od_raw, 10);
+	if (speed !== 1) od = (79.5 - Math.min(79.5, Math.max(19.5, 79.5 - Math.ceil(6 * od))) / speed) / 6;
 
 	return {
 		cs: Math.round(cs * 10) / 10,
