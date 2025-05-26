3c8733f209592024482f884ae5fedc8b097be0d6
Dao Heng Liu <daohengliu@gmail.com>
2025-05-26 16:07:45 +0100

    rename `gameplay` to `main` in graphics

--- src/graphics/main.ts
+++ src/graphics/main.ts
@@ -1,6 +1,6 @@
 import { createHead } from '@vueuse/head';
 import { createApp } from 'vue';
-import App from './gameplay/main.vue';
+import App from './main/main.vue';
 // Import dev mode styling (will only activate if OSUFR_DEV_MODE is true)
 import '@4wc-stream-overlay/browser_shared/dev-mode';
 
