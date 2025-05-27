5a6c8f6862033c09b84d3e28f425c671d4a0f803
Dao Heng Liu <daohengliu@gmail.com>
2025-05-27 20:01:46 +0100

    add mappool graphic

---
+++ src/graphics/mappool.ts
@@ -0,0 +1,11 @@
+import { createHead } from '@vueuse/head';
+import { createApp } from 'vue';
+import App from './mappool/main.vue';
+
+// Import dev mode styling (will only activate if OSUFR_DEV_MODE is true)
+import '@4wc-stream-overlay/browser_shared/dev-mode';
+
+const app = createApp(App);
+const head = createHead();
+app.use(head);
+app.mount('#app');
